76062980afefb66a30cf2f0ec5981cc8ff50c264
Euge <1507064225@qq.com>
2024-08-28 17:22:09 +0800

    update setup info

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ with open('setup_requirements.txt') as f:
     requirements = f.read().splitlines()
 
 packages = find_packages(
-    exclude=["waifuset.ui", "waifuset.ui.*"]
+    exclude=["waifui", "waifui.*"]
 )
 
 setup(
     name='waifuset',
-    version='0.2.1dev0',
+    version='0.2.2',
     packages=packages,
     include_package_data=True,
-    description='Image caption tools',
+    description='tools for text-to-image dataset',
     long_description='',
     author='euge',
     author_email='1507064225@qq.com',
@@ -22,6 +22,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
